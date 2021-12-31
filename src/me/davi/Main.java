4b439b171396824dc8d1f7b3c8de28e3cba85f7b
davia <davia@192.168.10.2>
2021-12-31 00:52:50 -0300

    by daviziih

--- src/me/davi/Main.java
+++ src/me/davi/Main.java
@@ -3,15 +3,16 @@ package me.davi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
+
 	public static Main pl;
-	
+
 	@Override
 	public void onEnable() {
-	}
-	
-	@Override
-	public void onDisable() {
+		pl = this;
 	}
 
+	@Override
+	public void onDisable() {
+		pl = null;
+	}
 }
